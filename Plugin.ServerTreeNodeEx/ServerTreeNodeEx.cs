83c70c25cde74f83fdd4dd11f65929a25a38e409
j0hnh <johnhfly@163.com>
2017-02-09 14:06:33 +0800

    init

---
+++ Plugin.ServerTreeNodeEx/ServerTreeNodeEx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml;
+using RdcMan;
+
+namespace Plugin.ServerTreeNodeEx
+{
+
+    [Export(typeof(RdcMan.IPlugin))]
+    public class ServerTreeNodeEx : RdcMan.IPlugin
+    {
+        public void PostLoad(RdcMan.IPluginContext context)
+        {
+            var nodeDClickEvent = context.Tree.GetType().GetEvent("NodeMouseDoubleClick");
+            nodeDClickEvent.AddEventHandler(context.Tree, new TreeNodeMouseClickEventHandler((s, ee) =>
+            {
+                var server = (ee.Node as RdcMan.Server);
+                if (server != null && server.IsConnected)
+                {
+                    server.GetType().InvokeMember("GoFullScreen", BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public, null, server, null);
+                }
+            }));
+
+            var nodeClickEvent = context.Tree.GetType().GetEvent("NodeMouseClick");
+            nodeClickEvent.AddEventHandler(context.Tree, new TreeNodeMouseClickEventHandler((s, ee) =>
+            {
+                var server = (ee.Node as RdcMan.Server);
+                if (server != null)
+                {
+                    var hit = (s as TreeView).HitTest(ee.Location);
+                    if (ee.Button == MouseButtons.Left && hit.Location == TreeViewHitTestLocations.Image)
+                    {
+                        if (server.IsConnected) server.Disconnect();
+                        else server.Reconnect();
+                    }
+                }
+            }));
+        }
+
+        public void OnContextMenu(ContextMenuStrip contextMenuStrip, RdcMan.RdcTreeNode node)
+        {
+            //if (node != null)
+            //{
+            //    contextMenuStrip.Items.Insert(0, new DelegateMenuItem("Disconnect", MenuNames.SessionDisconnect, new Action(node.Disconnect)));
+            //}
+        }
+        public void OnDockServer(RdcMan.ServerBase server)
+        {
+        }
+
+        public void OnUndockServer(RdcMan.IUndockedServerForm form)
+        {
+        }
+
+        public void PreLoad(RdcMan.IPluginContext context, System.Xml.XmlNode xmlNode)
+        {
+        }
+
+        public XmlNode SaveSettings()
+        {
+            //var x = new System.Xml.XmlDocument();
+            //return x.CreateNode(XmlNodeType.Attribute, "path", "Remote");
+            return null;
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Plugin for Remote Desktop Connection Manager #
+
+A RDCMan Plugin,Server node enhancements, add double-click full-screen functionality and one-key connect / disconnect function.
+
+### double-click full-screen
+when server is connected, double click the server node, the current server will fullscreen
+
+### one-key connect/disconnect
+simplely click the icon of the server node, if the server is not connected, it will auto connect, vice versa.
